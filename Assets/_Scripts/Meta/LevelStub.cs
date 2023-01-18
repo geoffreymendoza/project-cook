1e0ead311e71f0db56e81aaef576021fbd02fcfa
Dyep <geoffrey.mendoza@ciit.edu.ph>
2023-01-18 14:23:30 +0800

    -added debugger option for levels

--- Assets/_Scripts/Meta/LevelStub.cs
+++ Assets/_Scripts/Meta/LevelStub.cs
@@ -3,8 +3,16 @@ using UnityEngine;
 
 public class LevelStub : MonoBehaviour
 {
+    [SerializeField] private bool _isDebugging;
+    [SerializeField] private LevelType _levelToDebug;
+    
     private void Start()
     {
+        if (_isDebugging)
+        {
+            LevelManager.LevelToLoad(_levelToDebug);
+        }
+        
         LevelManager.SpawnLevelToScene();
     }
 }
